b01e7866530178b79dd93370ed8adb4879cda615
Amadile <108079452+amadile@users.noreply.github.com>
2024-07-10 00:57:10 -0700

    Add files via upload

---
+++ groupWork.py
@@ -0,0 +1,30 @@
+#Taking user input in python
+#The use of input() function
+value=input("Enter your value:")
+print(value)
+
+name=input("Enter your name:")
+print(name)
+
+#program to check input type in python
+num=input("Enter the number")
+print(num)
+
+name1=input("Enter the name")
+print(name1)
+
+#printing the type of input value
+print("type of number: ", type(num))
+print("type of name: ", type(name1))
+
+num = int(input("Enter a number: ")) 
+print(num, " ", type(num)) 
+
+			
+floatNum = float(input("Enter a decimal number: ")) 
+print(floatNum, " ", type(floatNum)) 
+
+#python module is a file that contains inbuilt functions, classes ,its and variables
+import calc
+print(calc.add(10, 2))
+
