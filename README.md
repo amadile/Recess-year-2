2333b43b6075972bac4bdb1b50d982a2fa170f84
Amadile <amadilemajid10@gmail.com>
2024-07-10 00:35:15 -0700

    updated my folder

--- README.md
+++
@@ -1,2 +0,0 @@
-# Recess-year-2
-The folder to do recess assignments for year two software engineering
--- views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import SoilMoistureReading
-
-# Create your views here.
-def moisture_list(request):
-    soil_readings = SoilMoistureReading.objects.all().order_by('-timestamp')
-    return render(request, 'planApp/moisture_list.html', {'soil_readings': soil_readings})
-
